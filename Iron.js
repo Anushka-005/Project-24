01a037a4346a7b80d996cefc8b599bbfc537e818
Anushka-005 <80161917+Anushka-005@users.noreply.github.com>
2021-10-12 18:37:24 +0530

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,25 @@
+class Iron {
+    constructor(x, y) {
+      var options = {
+        'density':30,
+        'friction': 3,
+        'restitution':0.8
+      };
+      this.body = Bodies.rectangle(x, y, 100, 50, options);
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke("black");
+      fill("brown");
+      rectMode(CENTER)
+      rect(0,0,100,50);
+      pop();
+    };
+  };
